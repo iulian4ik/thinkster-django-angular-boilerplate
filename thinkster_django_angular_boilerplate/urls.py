977ae994c7ad88beb2f747313121cd809ff25517
iulian gulea <iulian.gulea@gmail.com>
2014-11-22 17:23:54 +0200

    Authentication and part of login functionality

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,8 +1,21 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.views.generic.base import TemplateView
+
+from rest_framework import routers
+
+from authentication.views import AccountViewSet
+from authentication.views import LoginView
+
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
 
-    url('^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+
+    # url('^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url('^', TemplateView.as_view(template_name='index.html'), name='index'),
 )
